e1918a946b62072e13b7dce8bda5c456f81157fc
akkerman_e <estherakkerman@hotmail.com>
2023-01-24 12:14:17 -0600

    Added random users to display

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function() {
+    const userRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await userRequest.json();
+    const userResults = data.results;
+    displayUsers(userResults);
+};
+
+const displayUsers = function (userResults) {
+    randomFolks.innerHTML = "";
+
+    for (let user of userResults) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageUrl = user.picture.medium;
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `<h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${imageUrl} alt="User avatar" />`;
+        randomFolks.append(userDiv);
+    }
+};
+
+getData();
